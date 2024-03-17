1611edebc20c6644e68521a8e2f6bd77a82a7629
YuriiChlek <yurichlek@gmail.com>
2024-03-18 01:40:40 +0200

    Start

---
+++ src/app/bootstrap/main/main.server.ts
@@ -0,0 +1,7 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent} from "../root/app.component";
+import { config } from '../root/app.config.server';
+
+const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+export default bootstrap;
