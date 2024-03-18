f2167cbe4223cc9282a19bd62c66041e23649a58
YuriChlek <yurichlek@gmail.com>
2024-03-18 15:30:46 +0200

    Tailwind and apolo graphql were added

--- src/app/bootstrap/root/app.component.ts
+++ src/app/bootstrap/root/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { AppModuleMain } from "../../modules/app-main/app.module.main";
-import { AppMainContentComponent } from "../../theme/app-theme/app-main-content/app-main-content.component";
+import { AppModuleMain } from "../../modules/app-main-module/app.module.main";
+import { AppMainContentComponent } from "../../theme/app-theme-components/app-main-content/app-main-content.component";
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppModuleMain, AppMainContentComponent],
+  imports: [AppModuleMain, AppMainContentComponent, AppMainContentComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
